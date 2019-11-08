7f6baf4f673be1b29fcad314abf777424956e430
Ximon Eighteen <3304436+ximon18@users.noreply.github.com>
2019-11-08 15:05:06 +0100

    FIX: Don't bail out in some odd cases where capturing the process output is not sufficient to prevent the script being aborted, instead forcibly temporarily disable set -e.
    Also improved the output retry log output a bit.

--- terraform/krill-integration-demo/lib/docker/relyingparties/base/my_funcs.sh
+++ terraform/krill-integration-demo/lib/docker/relyingparties/base/my_funcs.sh
@@ -5,6 +5,11 @@ COLOUR_GREEN="\e[92m"
 COLOUR_DARK_GRAY="\e[90m"
 COLOUR_LIGHT_YELLOW="\e[93m"
 
+DEF_FAIL_STR="${COLOUR_RED}FAIL${COLOUR_DEFAULT}"
+DEF_OKAY_TO_FAIL_STR="${COLOUR_LIGHT_YELLOW}OKAY${COLOUR_DEFAULT}"
+DEF_WARN_UNTIL_FAIL_STR="${COLOUR_DARK_GRAY}WARN${COLOUR_DEFAULT}"
+DEF_OKAY_STR="${COLOUR_GREEN}OKAY${COLOUR_DEFAULT}"
+
 # Usage: <LOG MESSAGE>...
 my_log() {
     OPTS="${BANNER:-}"
@@ -21,39 +26,49 @@ my_try_cmd() {
     my_retry --okay-to-fail 1 0 $*
 }
 
-# Usage: [--okay-to-fail] <MAX_TRIES> <SLEEP_TIME_BETWEEN_TRIES> <COMMAND> [<ARG>...]
+# Usage: [--okay-to-fail|--retry-is-unexpected] <MAX_TRIES> <SLEEP_TIME_BETWEEN_TRIES> <COMMAND> [<ARG>...]
 # Retry a command up to N times with a sleep of M seconds between retries
 # If --okay-to-fail the result will not be logged as OKAY or FAIL nor will
 # failure stdout/stderr be logged.
 my_retry() {
-    FAIL_STR="${COLOUR_RED}FAIL"
+    FAIL_STR="${DEF_WARN_UNTIL_FAIL_STR}"
     OKAY_TO_FAIL=0
+    WARN_UNTIL_FAIL=1
     if [[ $# -ge 1 && "$1" == "--okay-to-fail" ]]; then
         shift 1
         OKAY_TO_FAIL=1
-        FAIL_STR="${COLOUR_LIGHT_YELLOW}OKAY"
+        FAIL_STR="${DEF_OKAY_TO_FAIL_STR}"
+    elif [[ $# -ge 1 && "$1" == "--retry-is-unexpected" ]]; then
+        shift 1
+        WARN_UNTIL_FAIL=0
+        FAIL_STR="${DEF_FAIL_STR}"
     fi
     MAX_TRIES=$1
     SLEEP_BETWEEN=$2
     shift 2
+    CMD_TO_RUN=$@
 
     TRIES=0
     while true; do
         (( TRIES=TRIES+1 ))
-        my_log --no-eol "Try ${TRIES}/${MAX_TRIES}: $@: "
+        my_log --no-eol "Try ${TRIES}/${MAX_TRIES}: ${CMD_TO_RUN}: "
 
         # capture (and thus suppress) and merge STDOUT and STDERR
-        OUTPUT=$($@ 2>&1)
+        SAVED_BASH_SET_FLAGS=$-
+        [[ ${SAVED_BASH_SET_FLAGS} =~ e ]] && set +e
+        OUTPUT=$(${CMD_TO_RUN} 2>&1)
         RC=$?
+        set -${SAVED_BASH_SET_FLAGS}
 
         if [ ${RC} -eq 0 ]; then
-            my_log --cont "${COLOUR_GREEN}OKAY${COLOUR_DEFAULT}"
+            my_log --cont "${DEF_OKAY_STR}"
             echo "$OUTPUT"
             return 0
         else
-            MSG="${FAIL_STR}${COLOUR_DEFAULT}"
+            MSG="${FAIL_STR}"
             MSG="${MSG} (EXIT CODE ${RC})"
             if [ ${TRIES} -ge ${MAX_TRIES} ]; then
+                [[ ${OKAY_TO_FAIL} -eq 0 && ${WARN_UNTIL_FAIL} -eq 1 ]] && MSG="${DEF_FAIL_STR}"
                 my_log --cont "${MSG}"
                 if [ ${OKAY_TO_FAIL} -eq 0 ]; then
                     my_log --no-eol "${COLOUR_DARK_GRAY}"
@@ -62,7 +77,7 @@ my_retry() {
                 fi
                 return ${RC}
             else
-                my_log "${MSG} (next try in ${SLEEP_BETWEEN} seconds)"
+                my_log --cont "${MSG} (next try in ${SLEEP_BETWEEN} seconds)"
             fi
         fi
 
