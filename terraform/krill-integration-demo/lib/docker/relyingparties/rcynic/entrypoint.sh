9b1aa8bfe5f7db752a5ad6210e9d16f9ee248451
Ximon Eighteen <3304436+ximon18@users.noreply.github.com>
2019-11-08 15:17:37 +0100

    FIX: When manually processing Rcynic ROAs in the DB (a) exclude those of the parent when Krill is not the parent.

--- terraform/krill-integration-demo/lib/docker/relyingparties/rcynic/entrypoint.sh
+++ terraform/krill-integration-demo/lib/docker/relyingparties/rcynic/entrypoint.sh
@@ -27,10 +27,34 @@ rcynic-text ${DATA_DIR}/validator.log.xml
 # then parse them using the Rcynica 'parse_roa' CLI tool, and then use sed to
 # reformat the brief ROA details into the JSON format expected by test_krill.sh
 my_log "Dumping received ROAs in the format expected by test_krill.sh"
-TMPDIR=$(mktemp -d)
-cd ${TMPDIR}
-sqlite3 ${RCYNIC_DB_PATH} "SELECT writefile(id, der) FROM rcynicdb_rpkiobject WHERE uri LIKE '%krill%.roa'" >/dev/null
-echo -n 'TEST OUT: { "roas": ['
-print_roa --brief * | sed -e 's|^\([0-9]\+\) \([^/]\+\)/\([0-9]\+\)|{ "asn": "AS\1", "prefix": "\2/\3", "maxLength": \3, "ta": "ta" }|' | paste -sd ',' - | sed -e 's|$|] }|'
-cd /
-rm -Rf ${TMPDIR}
+
+DB_TABLE="rcynicdb_rpkiobject"
+DB_URI_FIELD="uri"
+DB_ROA_PATTERN="%.roa"
+DB_INCLUDE_SUB_QUERY="${DB_URI_FIELD} LIKE '${DB_ROA_PATTERN}'"
+DB_EXCLUDE_SUB_QUERY=""
+if [ "${KRILL_USE_TA}" != "true" ]; then
+    # Exclude ROAs attributed to the "parent"
+    # I'm sure this is not really the right way to do this but it will do for
+    # now.
+    CER_URI=$(grep -F .cer ${TAL_DIR}/ta.tal)
+    EXT_DOM=$(echo $CER_URI | cut -d '/' -f 3)
+    DB_EXCLUDE_SUB_QUERY=" AND ${DB_URI_FIELD} NOT LIKE '%${EXT_DOM}%'"
+fi
+
+DB_MATCH_CRITERIA="${DB_INCLUDE_SUB_QUERY}${DB_EXCLUDE_SUB_QUERY}"
+DB_SELECT_COUNT="SELECT count(*) FROM ${DB_TABLE} WHERE ${DB_MATCH_CRITERIA}"
+ROA_COUNT=$(sqlite3 ${RCYNIC_DB_PATH} "${DB_SELECT_COUNT}")
+if [ ${ROA_COUNT} -le 0 ]; then
+    my_log "ERROR: Zero ROAs found in Rcynic DB with query: ${DB_SELECT_COUNT}"
+    echo 'TEST OUT: { "roas": []}'
+else
+    my_log "Found ${ROA_COUNT} ROAs in Rcynic DB with query: ${DB_SELECT_COUNT}"
+    TMPDIR=$(mktemp -d)
+    cd ${TMPDIR}
+    sqlite3 ${RCYNIC_DB_PATH} "SELECT writefile(id, der) FROM ${DB_TABLE} WHERE ${DB_MATCH_CRITERIA}" >/dev/null
+    echo -n 'TEST OUT: { "roas": ['
+    print_roa --brief * | sed -e 's|^\([0-9]\+\) \([^/]\+\)/\([0-9]\+\)|{ "asn": "AS\1", "prefix": "\2/\3", "maxLength": \3, "ta": "ta" }|' | paste -sd ',' - | sed -e 's|$|] }|'
+    cd /
+    #rm -Rf ${TMPDIR}
+fi
