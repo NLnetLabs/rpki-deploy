f9b78d1d550f3c846b5333922bba0d6cc96e3c76
Ximon Eighteen <3304436+ximon18@users.noreply.github.com>
2020-01-20 23:39:47 +0100

    FIX

--- run-tests/entrypoint.sh
+++ run-tests/entrypoint.sh
@@ -1,33 +1,35 @@
 #!/bin/bash
 set -euxo pipefail
 
+TF_STATE_PATH=${GITHUB_WORKSPACE}/tf.state
+REPORT_PATH=/tmp/report.html
+
 export TF_VAR_krill_build_path="${GITHUB_WORKSPACE}/krill"
 export TF_VAR_ssh_key_path="${INPUT_SSH-KEY-PATH}"
 export TF_VAR_do_token="${INPUT_DO-TOKEN}"
 export TF_VAR_size='s-4vcpu-8gb'
 export TF_VAR_domain='krill.cloud'
 export TF_VAR_tags='["rpki-deploy"]'
-MODE="${INPUT_MODE}"
+export TF_VAR_run_tests="false"
 
 echo "::add-mask::${TF_VAR_do_token}"
 
 cd "${TF_DIR}"
 
-case $MODE in
-    deploy)
-        export TF_VAR_run_tests="false"
-        terraform apply -lock=false -auto-approve
-        ;;
-
+case ${INPUT_MODE} in
     run-tests)
         export TF_VAR_run_tests="true"
+        # deliberate fall through
+        ;&
+
+    deploy)
         terraform init
-        terraform apply -lock=false -auto-approve
-        mv /tmp/report.html ${GITHUB_WORKSPACE}/
+        terraform apply -state ${TF_STATE_PATH} -lock=false -auto-approve
+        [ -f ${REPORT_PATH} ] && mv ${REPORT_PATH} ${GITHUB_WORKSPACE}/
         ;;
 
     undeploy)
-        terraform destroy -auto-approve
+        terraform destroy -state ${TF_STATE_PATH} -auto-approve
         ;;
 
     *)
