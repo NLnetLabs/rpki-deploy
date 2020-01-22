c28d62849b464fe20c83aa930305deb0f9639c04
ximon18 <3304436+ximon18@users.noreply.github.com>
2020-01-22 18:17:06 +0100

    FIX: Git come to revision is not possible.

--- terraform/krill-e2e-test/lib/post/main.tf
+++ terraform/krill-e2e-test/lib/post/main.tf
@@ -66,7 +66,8 @@ resource "null_resource" "setup" {
 
             cd $TMPDIR
             [ -d python-binding ] && rm -R python-binding
-            git clone --depth 1 --branch 2ade90e https://github.com/rtrlib/python-binding.git && \
+            git clone --branch master https://github.com/rtrlib/python-binding.git && 
+            git checkout 2ade90e \
                 cd python-binding && \
                 pip3 install -r requirements.txt && \
                 python3 setup.py build && \
