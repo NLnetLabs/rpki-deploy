cc791017e8c0f06d795138f022fb492cba14c9b5
ximon18 <3304436+ximon18@users.noreply.github.com>
2020-02-24 16:26:01 +0100

    Added a TODO.

--- terraform/krill-e2e-test/tests/util/pytest.py
+++ terraform/krill-e2e-test/tests/util/pytest.py
@@ -7,6 +7,11 @@ import pprint
 #   - https://github.com/pytest-dev/pytest/blob/a176ff77bc679db0305abc360434c2ca15e12165/doc/en/example/simple.rst#making-test-result-information-available-in-fixtures
 # Requires pytest_runtest_makereport() function in conftest.py to create rep_call.
 def isfailed(request):
+    # TODO: there is no key 'call' for a failure during a fixture setup,
+    # and in the case of a parameterized test the nodeid is a startswith match
+    # to the first part of the key that is actually stored in the test report,
+    # so there is no exact match. How do we want to detect failure in this case
+    # so that we can trigger dumping of the docker logs?
     key = (request.node.nodeid, 'call')
     reports = request.node.module._test_reports
     return reports[key].outcome == 'failed' if key in reports else False
