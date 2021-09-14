501ab48da878fc5ec96d60941fe01cd296bcde26
Ximon Eighteen <3304436+ximon18@users.noreply.github.com>
2021-09-14 11:09:52 +0200

    Revert accidental local debug change.

--- terraform/krill-e2e-test/lib/docker/main.tf
+++ terraform/krill-e2e-test/lib/docker/main.tf
@@ -58,7 +58,7 @@ resource "null_resource" "setup_docker" {
     working_dir = var.krill_build_path
     command = <<-EOT
         set -eu
-        echo ${local.krill_build_cmd}
+        ${local.krill_build_cmd}
     EOT
   }
 
